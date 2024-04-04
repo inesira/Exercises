e4698998375be2493de9f3707907feeba39091bd
inesira <inesiradodi@gmail.com>
2024-04-04 20:05:01 +0200

    some modifications

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,16 +5,11 @@ import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient } from '@angular/common/http';
 
-import { MuscleService } from './muscle.service';
-import { ApiKeyInterceptor } from '../../interceptors/apikey.interceptor';
-
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes), 
     provideClientHydration(), 
     provideHttpClient(),
-    { provide: ApiKeyInterceptor},
-    { provide: MuscleService}
   ]
 };
